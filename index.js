cb245c0aba27cbe8ed06f226eeed523d0e5feb10
AironRuan <aironruan3@gmail.com>
2024-01-04 21:34:12 -0300

    Desafio do Herói

---
+++ index.js
@@ -0,0 +1,27 @@
+let nomeHeroi = prompt("Digite o seu nome herói:");
+let xpHeroi = parseInt(prompt("Digite a sua quantidade de experiência:"));
+
+let nivel;
+
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+console.log(`${nomeHeroi} está no nível: ${nivel}`);
+
+
+
